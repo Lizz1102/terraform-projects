135891a85913b3dd3e96302d6b610b575be14891
Lizz1102 <lizz05110@gmail.com>
2023-08-10 22:16:41 -0400

    Create gw, route and route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,43 @@
+# TODO: Destroy and confirm in AWS Console that resources are destroyed 
 resource "aws_vpc" "dev_tf_vpc" {
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support   = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
 
 resource "aws_subnet" "dev_tf_public_subnet" {
-  vpc_id     = aws_vpc.dev_tf_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.dev_tf_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
-        Name = "dev-public"
-    }
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "dev_internet_gw" {
+  vpc_id = aws_vpc.dev_tf_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev_public_rt" {
+  vpc_id = aws_vpc.dev_tf_vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.dev_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev_internet_gw.id
 }
